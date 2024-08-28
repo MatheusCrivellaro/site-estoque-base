0ccd7fef5297449df76009573378729d54bd5541
MatheusCrivellaro <128638676+MatheusCrivellaro@users.noreply.github.com>
2024-08-28 09:36:10 -0300

    commit financiamento

--- src/pages/financiamento/Financiamento.tsx
+++ src/pages/financiamento/Financiamento.tsx
@@ -1,16 +1,16 @@
-import './Financiamento.css';
-import ButtonSuspense from "../../components/ButtonSuspense/ButtonSuspense.tsx";
-import {FaArrowDown} from "react-icons/fa6";
-import CardPassoFinanciamento from "../../components/CardPassoFinanciamento/CardPassoFinanciamento.tsx";
+import { useRef } from "react";
+import { FaArrowDown } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
+import img_financiamento from "../../../public/image-financiamento.jpeg";
+import img_form from "../../../public/img-financiamento-formulario.png";
 import ArrowFinanciamento from "../../components/ArrowFinanciamento/ArrowFinanciamento.tsx";
+import ButtonSuspense from "../../components/ButtonSuspense/ButtonSuspense.tsx";
 import CardIntituicoesParceiras from "../../components/CardIntituicoesParceiras/CardIntituicoesParceiras.tsx";
-import useGetLogoEmpresas from "../../hooks/useGetLogoEmpresas.tsx";
+import CardPassoFinanciamento from "../../components/CardPassoFinanciamento/CardPassoFinanciamento.tsx";
 import FormDadosCliente from "../../components/FormDadosCliente/FormDadosCliente.tsx";
-import {usePostMail} from "../../hooks/usePostMail.tsx";
-import img_financiamento from "../../../public/image-financiamento.jpeg"
-import img_form from "../../../public/img-financiamento-formulario.png"
-import {useRef} from "react";
-import {useNavigate} from "react-router-dom";
+import useGetLogoEmpresas from "../../hooks/useGetLogoEmpresas.tsx";
+import { usePostMail } from "../../hooks/usePostMail.tsx";
+import './Financiamento.css';
 
 const Financiamento = () => {
 
@@ -34,21 +34,21 @@ const Financiamento = () => {
         let to = "matheuscriv@gmail.com";
         let subject = "Venda de veiculo, " + list[0]
         let text = textFormater(list[0], list[1], list[2], list[3], list[4], list[5], list[6]);
-        mutate({to, subject, text})
+        mutate({ to, subject, text })
         navigate("/form-concluido")
     }
 
     const listInstitucion = [
-        {name: "Bradesco", url: "bradesco.com.br", img: ""},
-        {name: "Banco Pan", url: "bancopan.com.br", img: ""},
-        {name: "BV", url: "bv.com.br", img: ""},
-        {name: "Creditas", url: "creditas.com.br", img: ""},
-        {name: "Daycoval", url: "daycoval.com.br", img: ""},
-        {name: "Digimais", url: "digimais.com.br", img: ""},
-        {name: "Itaú", url: "itau.com.br", img: ""},
-        {name: "omni", url: "omni.com.br", img: ""},
-        {name: "Santander", url: "santander.com.br", img: ""},
-        {name: "Safra", url: "safra.com.br", img: ""},
+        { name: "Bradesco", url: "bradesco.com.br", img: "" },
+        { name: "Banco Pan", url: "bancopan.com.br", img: "" },
+        { name: "BV", url: "bv.com.br", img: "" },
+        { name: "Creditas", url: "creditas.com.br", img: "" },
+        { name: "Daycoval", url: "daycoval.com.br", img: "" },
+        { name: "Digimais", url: "digimais.com.br", img: "" },
+        { name: "Itaú", url: "itau.com.br", img: "" },
+        { name: "omni", url: "omni.com.br", img: "" },
+        { name: "Santander", url: "santander.com.br", img: "" },
+        { name: "Safra", url: "safra.com.br", img: "" },
     ];
 
     return (
@@ -58,32 +58,32 @@ const Financiamento = () => {
                 <div className="passos-financiamento-container">
                     <h1>Fazer um financiamento</h1>
                     <div className="passos-financiamento">
-                        <CardPassoFinanciamento img={img_financiamento} title="1º passo - Pesquisa" description="Pesquisamos por taxas dos bancos com base nos seus dados pessoais, dados do veículo e valor a ser financiado."/>
+                        <CardPassoFinanciamento img={img_financiamento} title="1º passo - Pesquisa" description="Pesquisamos por taxas dos bancos com base nos seus dados pessoais, dados do veículo e valor a ser financiado." />
                         <ArrowFinanciamento />
-                        <CardPassoFinanciamento img={img_financiamento} title="2º passo - Aprovação" description="Você escolhe a melhor opção e passamos todos os seus dados ao banco para te aprovar."/>
+                        <CardPassoFinanciamento img={img_financiamento} title="2º passo - Aprovação" description="Você escolhe a melhor opção e passamos todos os seus dados ao banco para te aprovar." />
                         <ArrowFinanciamento />
-                        <CardPassoFinanciamento img={img_financiamento} title="Pronto" description="Agora é só assinar o contrato conosco e realizar o pagamento."/>
+                        <CardPassoFinanciamento img={img_financiamento} title="Pronto" description="Agora é só assinar o contrato conosco e realizar o pagamento." />
                     </div>
                     <div className="button-container-passoas-financiameto">
                         <button onClick={handleScrollForm}>
                             Fazer um financiamento
-                            <FaArrowDown className="button-icon-passoas-financiameto"/>
+                            <FaArrowDown className="button-icon-passoas-financiameto" />
                         </button>
                     </div>
                 </div>
                 <div className="passos-financiamento-container">
                     <h1>Fazer um refinanciamento</h1>
                     <div className="passos-financiamento">
-                        <CardPassoFinanciamento img={img_financiamento} title="Dinheiro no bolso" description="Faça empréstimos, deixando seu veículo como forma de garantia."/>
+                        <CardPassoFinanciamento img={img_financiamento} title="Dinheiro no bolso" description="Faça empréstimos, deixando seu veículo como forma de garantia." />
                         <ArrowFinanciamento />
-                        <CardPassoFinanciamento img={img_financiamento} title="Aprovação" description="Pré-aprovação em minutos, após finalizar cadastro."/>
+                        <CardPassoFinanciamento img={img_financiamento} title="Aprovação" description="Pré-aprovação em minutos, após finalizar cadastro." />
                         <ArrowFinanciamento />
-                        <CardPassoFinanciamento img={img_financiamento} title="Por que contratar?" description="Facilidade em emprestar quantias maiores com prazo maior de pagamento, além de taxas competitivas."/>
+                        <CardPassoFinanciamento img={img_financiamento} title="Por que contratar?" description="Facilidade em emprestar quantias maiores com prazo maior de pagamento, além de taxas competitivas." />
                     </div>
                     <div className="button-container-passoas-financiameto">
                         <button onClick={handleScrollForm}>
                             Fazer refinanciamento
-                            <FaArrowDown className="button-icon-passoas-financiameto"/>
+                            <FaArrowDown className="button-icon-passoas-financiameto" />
                         </button>
                     </div>
                 </div>
@@ -91,7 +91,7 @@ const Financiamento = () => {
                     <h1>Instituições parceiras</h1>
                     <div className="container-cards-instituicoes-financiamento">
                         {listInstitucion.map((value, index) =>
-                            <CardIntituicoesParceiras img={getLogoUrl(value.url)} titulo={value.name} key={index}/>
+                            <CardIntituicoesParceiras img={getLogoUrl(value.url)} titulo={value.name} key={index} />
                         )}
                     </div>
                 </div>
