e54653ead28475f329efbe4325f82dab8fb0809b
MatheusCrivellaro <matheuscriv@gmail.com>
2024-08-30 19:13:36 -0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx';
 import './index.css';
 
+import { register } from "swiper/element/bundle";
 
+register();
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
 const client = new QueryClient();
 
